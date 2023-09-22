2c20e2cc9bfacae9406cebf9d76e76403089c294
Elwoodbond <115675969+Elwoodbond@users.noreply.github.com>
2023-09-22 15:16:18 -0400

    web app created + get blockchain function

--- blockchain.py
+++ blockchain.py
@@ -69,6 +69,30 @@ class Blockchain:
             
         return True
     
+#creating web app
+
+app = Flask(__name__) 
+
+#get blockchain
+
+blockchain = Blockchain()
     
-    
-# Mining the blockchain
+@app.route('/mine_block', methods = ['GET'])
+
+def mine_block(): 
+        previous_block = blockchain.get_previous_block()
+        previous_proof = previous_block['proof']
+        proof = blockchain.proof_of_work(previous_proof)
+        
+        previous_hash = blockchain.hash(previous_block)
+        
+        block = blockchain.create_block(proof, previous_hash)
+        
+        response = {'message' : 'Congratulations, you just mined a block! Pick it up!',
+                    'index' : block['index'],
+                    'timestamp' : block['timestamp'],
+                    'proof' : block['proof'],
+                    'previous_hash' : block['previous_hash']}
+        
+        return jsonify(response), 200
+
